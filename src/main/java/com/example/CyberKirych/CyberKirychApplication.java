8e13cb6ef5cf325430238024bfbc4501ace6a9a0
diespensary <diespensary@icloud.com>
2025-03-01 14:58:00 +0400

    init commit

---
+++ src/main/java/com/example/CyberKirych/CyberKirychApplication.java
@@ -0,0 +1,13 @@
+package com.example.CyberKirych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberKirychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberKirychApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CyberKirych/CyberKirychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CyberKirych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberKirychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
